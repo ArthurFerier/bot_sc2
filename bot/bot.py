0e19ca94fdd63954605aa78b1d211142f6c0a304
ArthurFerier <arthur.ferier1@gmail.com>
2021-01-22 12:06:30 +0100

    2.0.0 : bot very easy defeated

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,18 @@
 from sc2 import BotAI, Race
+from sc2.ids.unit_typeid import UnitTypeId  # we need to import unitTypeId like this ?
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.upgrade_id import UpgradeId
+from sc2.unit import Unit
+from sc2.units import Units
+from sc2.position import Point2
+from sc2.player import Bot, Computer
+from sc2.data import Race
 
 
 class CompetitiveBot(BotAI):
     NAME: str = "CompetitiveBot"
     """This bot's name"""
-    RACE: Race = Race.Terran
+    RACE: Race = Race.Protoss
     """This bot's Starcraft 2 race.
     Options are:
         Race.Terran
@@ -17,9 +25,161 @@ class CompetitiveBot(BotAI):
         print("Game started")
         # Do things here before the game starts
 
+
     async def on_step(self, iteration):
         # Populate this function with whatever your bot should do!
+
+        # send the worker to collect resources when doing nothing
+        await self.distribute_workers()
+
+        # build workers
+        # self.do(self.townhalls.idle.first.train(UnitTypeId.SCV)) # not good but I tried
+        await self.build_workers()
+
+        await self.build_pylons()
+
+        await self.build_gateway()
+
+        await self.build_gas()
+
+        await self.build_cyber_core()
+
+        await self.train_stalkers()
+
+        await self.build_four_gates()
+
+        await self.chrono()
+
+        await self.warpgate_research()
+
+        await self.attack()
+
         pass
+
+
+    async def build_workers(self):
+        nexus = self.townhalls.ready.random
+        if (
+            self.can_afford(UnitTypeId.PROBE) and  # UnitTypeId.PROBE = worker
+            nexus.is_idle and  # not doing anything and
+            # amount of workers < number of townhalls times 22 (22 workers per townhalls is a good mean)
+            self.workers.amount < self.townhalls.amount * 22
+        ):
+            # we are here but no workers are being built ...
+            nexus.train(UnitTypeId.PROBE)
+
+
+    async def build_pylons(self):
+        nexus = self.townhalls.ready.random
+        position = nexus.position.towards(self.enemy_start_locations[0], 10)
+
+        if (
+            # if we have less than 3 spaces left for units
+            self.supply_left < 3 and
+            # verify that we are not already building a pylon
+            self.already_pending(UnitTypeId.PYLON) == 0 and
+            self.can_afford(UnitTypeId.PYLON)
+        ):
+            await self.build(UnitTypeId.PYLON, near=position)
+
+
+    async def build_gateway(self):
+        if (
+            self.structures(UnitTypeId.PYLON).ready and
+            self.can_afford(UnitTypeId.GATEWAY) and
+                # if we don't have agateway already
+            not self.structures(UnitTypeId.GATEWAY)
+        ):
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+
+
+    async def build_gas(self):
+        # if we have a gateway => not necessary but so we have like an order of buildings
+        if self.structures(UnitTypeId.GATEWAY):
+            for nexus in self.townhalls.ready:
+                # we don't want to build an assimilator at places that are not close to the nexus
+                vgs = self.vespene_geyser.closer_than(15, nexus)
+                for vg in vgs:
+                    if not self.can_afford(UnitTypeId.ASSIMILATOR):
+                        break
+                    worker = self.select_build_worker(vg.position)
+                    if worker is None:
+                        break
+                    # if we don't have any gas building
+                    # or any gas buildings that are closer than 1 to or vespene geyser
+                    if not self.gas_buildings or not self.gas_buildings.closer_than(1, vg):
+                        worker.build(UnitTypeId.ASSIMILATOR, vg)
+                        # don't know what it is doing
+                        worker.stop(queue=True)
+
+    async def build_cyber_core(self):
+        # we only need to build one cyber core
+        if self.structures(UnitTypeId.PYLON).ready:
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            if self.structures(UnitTypeId.GATEWAY).ready:
+                # if no cyber core, then build one
+                if not self.structures(UnitTypeId.CYBERNETICSCORE):
+                    if (
+                        self.can_afford(UnitTypeId.CYBERNETICSCORE) and
+                        self.already_pending(UnitTypeId.CYBERNETICSCORE) == 0
+                    ):
+                        await self.build(UnitTypeId.CYBERNETICSCORE, near=pylon)
+
+
+    async def train_stalkers(self):
+        for gateway in self.structures(UnitTypeId.GATEWAY):
+            if (
+                self.can_afford(UnitTypeId.STALKER) and
+                gateway.is_idle
+            ):
+                gateway.train(UnitTypeId.STALKER)
+
+
+    # build the rest of the gateway for the four gateway strategy
+    # (absolutely no idea of the strategy in question)
+    async def build_four_gates(self):
+        if (
+            self.structures(UnitTypeId.PYLON).ready and
+            self.can_afford(UnitTypeId.GATEWAY) and
+            self.structures(UnitTypeId.GATEWAY).amount < 4
+        ):
+            pylon = self.structures(UnitTypeId.PYLON).ready.random
+            await self.build(UnitTypeId.GATEWAY, near=pylon)
+
+
+    # to use the chrono boost of the protoss when we have energy
+    # when we have nothing, we want to chrono boost the nexus to build workers
+    # then we want to chrono boost our army to build it faster => not implemented yet
+    async def chrono(self):
+        if self.structures(UnitTypeId.PYLON):
+            nexus = self.townhalls.ready.random
+            if (
+                not self.structures(UnitTypeId.CYBERNETICSCORE).ready
+                and self.structures(UnitTypeId.PYLON).amount > 0
+            ):
+                if nexus.energy > 50:
+                    nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus)
+
+
+    async def warpgate_research(self):
+        if (
+            self.structures(UnitTypeId.CYBERNETICSCORE).ready and
+            self.can_afford(AbilityId.RESEARCH_WARPGATE) and
+            self.already_pending_upgrade(UpgradeId.WARPGATERESEARCH) == 0
+        ):
+            cybercore = self.structures(UnitTypeId.CYBERNETICSCORE).ready.first
+            cybercore.research(UpgradeId.WARPGATERESEARCH)
+
+
+    async def attack(self):
+        stalkercount = self.units(UnitTypeId.STALKER).amount
+        stalkers = self.units(UnitTypeId.STALKER).ready.idle
+
+        for stalker in stalkers:
+            if stalkercount > 4:
+                stalker.attack(self.enemy_start_locations[0])
+
 
     def on_end(self, result):
         print("Game ended.")
