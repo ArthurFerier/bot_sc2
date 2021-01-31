6f2660334d5b55df082e11838aee58ccc429e6e8
ArthurFerier <arthur.ferier1@gmail.com>
2021-01-31 12:14:26 +0100

    1.6.0 : chrono, build army, standby army to modify

--- bot/bot.py
+++ bot/bot.py
@@ -27,18 +27,15 @@ class CompetitiveBot(BotAI):
     
     """
 
-
-
     def __init__(self):
         BotAI.__init__(self)
         self.number_pylons = 0
         self.can_construct = True
-
+        self.can_train = True
 
     async def on_start(self):
         print("Game started")
         # Do things here before the game starts
-
 
     async def on_step(self, iteration):
         # Populate this function with whatever your bot should do!
@@ -51,12 +48,17 @@ class CompetitiveBot(BotAI):
         await self.build_pylons()
         await self.build_vespene()
 
+        await self.chrono()
+
         await self.build_cyber_core()
+        await self.warp_research()
 
         await self.build_first_expansion()
 
-        pass
+        await self.train_stalkers()
+        await self.standby_army()
 
+        pass
 
     # fine function, I don't think there is anything to add (because of the distribute workers)
     async def build_workers(self):
@@ -72,14 +74,14 @@ class CompetitiveBot(BotAI):
     async def build_pylons(self):
 
         if (
-            # if we have less than 4 spaces left for units
+                # if we have less than 4 spaces left for units
                 #  => implement good functions to see if it is the case
-            self.supply_left < 5 + int(self.time/60) and
-            # verify that we are not already building a pylon
-            self.already_pending(UnitTypeId.PYLON) == 0 and
-            self.can_afford(UnitTypeId.PYLON) and
-            # supply_cap at 200
-            self.supply_cap < 200
+                self.supply_left < 5 + int(self.time / 60) and
+                # verify that we are not already building a pylon
+                self.already_pending(UnitTypeId.PYLON) == 0 and
+                self.can_afford(UnitTypeId.PYLON) and
+                # supply_cap at 200
+                self.supply_cap < 200
         ):
             nexus = self.townhalls.ready.random
             position = await self.find_pylon_pos(nexus)
@@ -117,6 +119,7 @@ class CompetitiveBot(BotAI):
 
         pos = nexus.position.random_on_distance([8, 15])
         for i in range(20):
+            print("essai n {}".format(i))
             # we want to spread the pylons
             # and not in the way of the workers gathering minerals
 
@@ -126,45 +129,41 @@ class CompetitiveBot(BotAI):
             if pos.distance_to_closest(pylons) > 15:
                 return pos
             pos = nexus.position.random_on_distance([8, 15])
-
+        print("les 20 positions n'ont pas été un succès")
         return pos
-
 
     async def distance_to_nexus(self, position):
         nexus = self.townhalls.closest_to(position)
         return nexus.position.distance_to_point2(position)
-
 
     async def build_gateways(self):
         # build first gateway
         if (
             self.structures(UnitTypeId.PYLON).ready
             and self.can_afford(UnitTypeId.GATEWAY)
-            and self.structures(UnitTypeId.GATEWAY).amount < 1
+            and self.structures(UnitTypeId.GATEWAY).amount
+            + self.structures(UnitTypeId.WARPGATE).amount < 1
             and self.can_construct
         ):
             pylon = self.structures(UnitTypeId.PYLON).ready.random
             position = pylon.position.random_on_distance((0, 6))  # askip 6.5 from liquidpedia
             await self.build(UnitTypeId.GATEWAY, near=position)
 
-
-
-
     async def build_vespene(self):
         if (
-            # build order : for the first assimilator
-            self.structures(UnitTypeId.PYLON)
-            and self.structures(UnitTypeId.GATEWAY)
-            and self.can_afford(UnitTypeId.ASSIMILATOR)
-            and self.can_construct
+                # build order : for the first assimilator
+                self.structures(UnitTypeId.PYLON)
+                and self.structures(UnitTypeId.GATEWAY)
+                and self.can_afford(UnitTypeId.ASSIMILATOR)
+                and self.can_construct
         ):
             # check if there are not already 2 assimilators per nexuses
             if self.structures(UnitTypeId.ASSIMILATOR).amount < self.townhalls.ready.amount * 2:
                 for nexus in self.townhalls.ready:
                     vggs = self.vespene_geyser.closer_than(15, nexus)
                     for vgg in vggs:
-                        worker = self.workers\
-                            .filter(lambda worker: worker.is_collecting or worker.is_idle)\
+                        worker = self.workers \
+                            .filter(lambda worker: worker.is_collecting or worker.is_idle) \
                             .closest_to(vgg.position)
 
                         if worker is None:
@@ -173,18 +172,41 @@ class CompetitiveBot(BotAI):
                             worker.build(UnitTypeId.ASSIMILATOR, vgg)
                             worker.stop(queue=True)
 
+    # todo : improve the overall
+    async def chrono(self):
+        if self.structures(UnitTypeId.PYLON):
+            nexus = self.townhalls.ready.random
+            if nexus.energy > 50:
+                if (
+                        self.structures(UnitTypeId.CYBERNETICSCORE).ready
+                ):
+                    # we want to boost it while he is not idle
+                    if not self.structures(UnitTypeId.CYBERNETICSCORE).ready.idle:
+                        cybercore = self.structures(UnitTypeId.CYBERNETICSCORE).ready.random
+                        nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, cybercore)
+                    else:
+                        # the conditions are fine, the prob is here under
+                        gateways = self.structures(UnitTypeId.GATEWAY).ready
+                        for gateway in gateways:
+                            # if there is a gateway that is not idle
+                            if not gateway.is_idle:
+                                nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, gateway)
+
+                else:
+                    if self.structures(UnitTypeId.PYLON).amount > 0:
+                        nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, nexus)
 
     async def build_cyber_core(self):
         if (
-            self.structures(UnitTypeId.ASSIMILATOR)
-            and not self.structures(UnitTypeId.CYBERNETICSCORE)
-            and not self.already_pending(UnitTypeId.CYBERNETICSCORE)
+                self.structures(UnitTypeId.ASSIMILATOR)
+                and not self.structures(UnitTypeId.CYBERNETICSCORE)
+                and not self.already_pending(UnitTypeId.CYBERNETICSCORE)
         ):
             if (
-                self.can_afford(UnitTypeId.CYBERNETICSCORE)
+                    self.can_afford(UnitTypeId.CYBERNETICSCORE)
             ):
                 # I want ot build the cybercore the furthest possible from the enemy
-                pylon = self.structures(UnitTypeId.PYLON)\
+                pylon = self.structures(UnitTypeId.PYLON) \
                     .ready.furthest_to(self.enemy_start_locations[0])
                 position = pylon.position.random_on_distance((0, 6))
 
@@ -203,12 +225,26 @@ class CompetitiveBot(BotAI):
                 # waiting to have enough money to construct the cybernetics
                 self.can_construct = False
 
+    async def warp_research(self):
+        if (
+                self.structures(UnitTypeId.CYBERNETICSCORE).ready
+                and not self.already_pending_upgrade(UpgradeId.WARPGATERESEARCH)
+        ):
+            if (
+                    self.can_afford(AbilityId.RESEARCH_WARPGATE)
+            ):
+                cybercore = self.structures(UnitTypeId.CYBERNETICSCORE).ready.first
+                cybercore.research(UpgradeId.WARPGATERESEARCH)
+
+                self.can_construct = True
+            else:
+                self.can_construct = False
 
     async def build_first_expansion(self):
-        if(
-            self.structures(UnitTypeId.CYBERNETICSCORE)
-            and self.townhalls.amount == 1
-            and not self.already_pending(UnitTypeId.NEXUS)
+        if (
+                self.structures(UnitTypeId.CYBERNETICSCORE)
+                and self.townhalls.amount == 1
+                and not self.already_pending(UnitTypeId.NEXUS)
         ):
             if self.can_afford(UnitTypeId.NEXUS):
                 await self.expand_now(building=UnitTypeId.NEXUS)
@@ -216,10 +252,27 @@ class CompetitiveBot(BotAI):
             else:
                 self.can_construct = False
 
+    async def train_stalkers(self):
+        # build first two stalkers
+        for getaway in self.structures(UnitTypeId.GATEWAY).ready:
+            print("we are here")
 
+            if(
+                self.can_train
+                and self.townhalls.amount == 2
+                and self.units(UnitTypeId.STALKER).amount < 2
+                and self.can_afford(UnitTypeId.STALKER)
+                and getaway.is_idle
+            ):
+                print("do we arrive here ?")
+                getaway.train(UnitTypeId.STALKER)
 
-
-
+    # todo : improve this function
+    async def standby_army(self):
+        town_to_defend = self.townhalls.closest_to(self.enemy_start_locations[0])
+        position = town_to_defend.position.towards(self.enemy_start_locations[0], 9)
+        for stalker in self.units(UnitTypeId.STALKER):
+            stalker.attack(position)
 
 
 
